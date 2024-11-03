ef7c088b51987655a234b76752a15947174e34c5
A24_Pr <2823659087@qq.com>
2024-11-03 20:04:05 +0800

    启动但是两个窗口

--- src/gazebo_tutorials/launch/launch_main.py
+++ src/gazebo_tutorials/launch/launch_main.py
@@ -1,20 +1,29 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
 
 def generate_launch_description():
     return LaunchDescription([
-        # 启动Gazebo
+        # 启动 Gazebo，并加载自定义的 world 文件
         ExecuteProcess(
-            cmd=['gazebo', '--verbose', '/home/jstone/Documents/graduation_project/T7/src/gazebo_tutorials/worlds/room4/room4.world', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+            cmd=['gazebo', 
+                 '--verbose', 
+                 '/home/jstone/Documents/graduation_project/T7/src/gazebo_tutorials/worlds/room4/room4.world', 
+                 '-s', 
+                 'libgazebo_ros_init.so', 
+                 '-s', 
+                 'libgazebo_ros_factory.so'],
             output='screen',
             name='gazebo'
         ),
-        # 启动TurtleBot3并设置初始位置
-        Node(
-            package='turtlebot3_gazebo',
-            executable='turtlebot3_gazebo',
-            output='screen',
-            parameters=[{'initial_pose_x': 1.0, 'initial_pose_y': 2.0, 'initial_pose_a': 0.0}]
+        # 启动 TurtleBot3
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join('/opt/ros/galactic/share/turtlebot3_gazebo/launch', 'turtlebot3_world.launch.py')),
+            launch_arguments={
+                'turtlebot3_model': 'burger',  # 根据您的模型选择
+                'use_sim_time': 'true',  # 使用仿真时间
+            }.items()
         ),
     ])
