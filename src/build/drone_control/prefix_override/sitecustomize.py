6f3ec68f96ba7032bd9d7f46d970223e8678956e
A24_Pr <2823659087@qq.com>
2025-01-08 20:34:51 +0800

    raw

---
+++ src/build/drone_control/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/jstone/Documents/graduation_project/9T/robot_army/src/install/drone_control'
