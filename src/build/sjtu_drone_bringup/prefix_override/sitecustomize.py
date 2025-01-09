6ea4ad4789cf326d31376933b4a2aa228d48392e
A24_Pr <2823659087@qq.com>
2025-01-09 20:51:03 +0800

    重构了项目结构

--- src/build/sjtu_drone_bringup/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jstone/Documents/graduation_project/9T/robot_army/src/install/sjtu_drone_bringup'
