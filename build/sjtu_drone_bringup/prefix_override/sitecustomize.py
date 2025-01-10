b83130b80ccb3d42ac4a517c8322e571fa0faacd
A24_Pr <2823659087@qq.com>
2025-01-10 23:37:34 +0800

    删除编译文件

--- build/sjtu_drone_bringup/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jstone/Documents/graduation_project/1P/robot_army/install/sjtu_drone_bringup'
