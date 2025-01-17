8c740267e1d529457817d68392600d78c1900e6f
A24_Pr <2823659087@qq.com>
2025-01-17 22:43:32 +0800

    在gazebo世界加入目标

--- src/turtlebot3_gazebo/launch/multi_robot_launch.py
+++ src/turtlebot3_gazebo/launch/multi_robot_launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     model_folder = 'turtlebot3_waffle'
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     robot_desc_path = os.path.join(get_package_share_directory("turtlebot3_gazebo"), "urdf", "turtlebot3_waffle.urdf")
-    world = os.path.join(get_package_share_directory('turtlebot3_gazebo'),'worlds','turtlebot3_house.world')
+    world = os.path.join(get_package_share_directory('turtlebot3_gazebo'),'worlds','house_with_cone.world')
     urdf_path1 = os.path.join(get_package_share_directory('turtlebot3_gazebo'),'models',model_folder+'_0','model.sdf')
     urdf_path2 = os.path.join(get_package_share_directory('turtlebot3_gazebo'),'models',model_folder+'_1','model.sdf')
     with open(robot_desc_path, 'r') as infp:
